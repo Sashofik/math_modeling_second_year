f90cb6caba98ddfe99d1b86e7264dc0a7231cc28
Sashofik <115093235+Sashofik@users.noreply.github.com>
2023-09-20 15:23:35 +0000

    1

--- lec1create.py
+++ lec1create.py
@@ -1,18 +1,23 @@
 class Ball(object):
     R = 6
-    def __init__(self,col):
+    def __init__(self, col):
         self.col = col
 
     def prin(self):
         print(self.col)
         print(Ball.R)
 
+    def initiat(self):
+        print("Инициирую вывод параметров...")
+        self.prin()
+
 ball_1 = Ball('red')
-print(ball_1.prin())
+#print(ball_1.prin())
 ball_1.R = 12
-print(ball_1.R)
-print(Ball.R)
-Ball.R = 44
-new_ball = Ball("blue")
-print(new_ball.R)
-#print(ball_1.info())
+ball_1.initiat()
+#print(ball_1.R)
+#print(Ball.R)
+#Ball.R = 44
+#new_ball = Ball("blue")
+#print(new_ball.R)
+
--- vr.py
+++ vr.py
@@ -1,1 +1,24 @@
-print("1")
+class Ball:
+
+    def drop(self):
+        print('Меня подкинули')
+
+    def kick(self):
+        print('Меня пнули')
+        self.drop()
+
+    def fail(self):
+        print('Прокололся :(')
+
+    # Метод класса: получает класс, но не экземпляр
+    def version(cls):
+        print('Версия мяча: 1.0')
+
+ball = Ball()
+
+ball.drop()
+ball.kick()
+ball.fail()
+ball.version()
+
+Ball.version(ball)
