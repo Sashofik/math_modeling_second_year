d157743e5d330be993ea844aa386ed6a19ae0382
Sashofik <115093235+Sashofik@users.noreply.github.com>
2023-10-04 16:24:09 +0000

    1

--- os3.py
+++ os3.py
@@ -2,35 +2,63 @@ class Tomato:
     states = ["Отсутствует", "Цветение", "Зеленый", "Красный"]
     def __init__(self, index):
         self.index = index
-        self.state = self.states[0]
-        self.col = 1
+        self.soz = 0
+        self.state = self.states[self.soz]
+        
     
     def grow(self):
-        
+        self.soz += 1
+        self.state = self.states[self.soz]
+
     def is_ripe(self):
-        
+        if self.soz == len(self.states):
+            return(True)
     
 class TomatoBush:
     def __init__(self, kolvo):
         self.kolvo = kolvo
+        self.tomatoes = []
         for i in range(kolvo):
-            globals()['tomato' + str(i)] = Tomato(i))
+            exec('self.tomato' + str(i)) = Tomato(i)
         for i in range(kolvo):
-            exec("self.tomatoes.append = {}".format('tomato' + str(i)))
+            exec("self.tomatoes.append('{}')".format("tomato" + str(i)))
 
     def grow_all(self):
+        for i in range(self.kolvo):
+            exec("self.tomato{}.grow".format(str(i)))
         
     def all_are_ripe(self):
+        a = 1
+        for i in range(self.kolvo):
+            exec("if self.tomato{}.is_ripe != True: a = 0".format(str(i)))
+            if a == 1:
+                return(True)
 
     def give_away_all(self):
-
+        self.tomatoes = []
+        for i in range(kolvo):
+            exec("del self.tomato{}".format(str(i)))
+        
 class Gardener:
     def __init__(self, name):
         self.name = name
         self.plant = TomatoBush(15)
     
     def work(self):
-        
-    def harvest(self):
+        self.plant.grow_all()
 
-    def knowledge_base(self):
+    def harvest(self):
+        if self.plant.all_are_ripe() == True:
+            self.plant.give_away_all()
+            print("Урожай собран")
+            exit(0)
+        else:
+            print("Не все плоды созрели")
+            self.work()
+
+ #   def knowledge_base(self):
+
+g = Gardener("Вася")
+
+while True:
+    g.harvest()
