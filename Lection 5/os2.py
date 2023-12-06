9887ad50dabccf61a4d8a1cbf4d3c7e509e5bfff
Sashofik <115093235+Sashofik@users.noreply.github.com>
2023-12-06 13:40:44 +0000

    12

--- Lection 5/os2.py
+++ Lection 5/os2.py
@@ -1,16 +1,14 @@
 def ug_chisol(niz, ver, ug_chis):
-    print(niz, ver)
-    print(int(ver / 2))
-    if ug_chis < int(ver / 2):
-        ver  = int(ver / 2)
+    if ug_chis < int((niz + ver) / 2):
+        ver  = int((niz + ver) / 2)
         ug_chisol(niz, ver, ug_chis)
 
-    elif ug_chis > int(ver / 2):
-        niz = int(ver / 2)
+    elif ug_chis > int((niz + ver) / 2):
+        niz = int((niz + ver) / 2)
         ug_chisol(niz, ver, ug_chis)
 
-    elif ug_chis == int(ver / 2):
-        print("Комп угадал число:", ug_chis, "Загаданное число: ", int(ver / 2))
+    elif ug_chis == int((niz + ver) / 2):
+        print("Комп угадал число:", ug_chis, "Загаданное число: ", int((niz + ver) / 2))
         
 
 zag_chisol = int(input("Введите число: "))
